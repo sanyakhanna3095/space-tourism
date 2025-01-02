377321b338b39eeb7b40a1fafae845e8c2ef0fc2
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-01-02 13:59:43 +0530

    Save files

--- dist/style.78032849.js
+++ dist/style.78032849.js
@@ -201,7 +201,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54032" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54981" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
